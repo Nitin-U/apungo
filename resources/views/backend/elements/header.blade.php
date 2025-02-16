e0c1352c9df8ee2842201b131b3712438c9543fd
anmolkoirala1313 <anmolkoirala1313@gmail.com>
2025-02-16 17:23:55 +0545

    CSS Updates

--- resources/views/backend/elements/header.blade.php
+++ resources/views/backend/elements/header.blade.php
@@ -76,75 +76,43 @@
                                 <div class="dropdown-menu-header border-bottom">
                                     <div class="dropdown-header d-flex align-items-center py-3">
                                         <h6 class="mb-0 me-auto">Shortcuts</h6>
-                                        <a href="javascript:void(0)" class="dropdown-shortcuts-add py-2" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Add shortcuts" data-bs-original-title="Add shortcuts"><i class="icon-base bx bx-plus-circle text-heading"></i></a>
+{{--                                        <a href="javascript:void(0)" class="dropdown-shortcuts-add py-2" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Add shortcuts" data-bs-original-title="Add shortcuts"><i class="icon-base bx bx-plus-circle text-heading"></i></a>--}}
                                     </div>
                                 </div>
                                 <div class="dropdown-shortcuts-list scrollable-container ps">
                                     <div class="row row-bordered overflow-visible g-0">
                                         <div class="dropdown-shortcuts-item col">
-                <span class="dropdown-shortcuts-icon rounded-circle mb-3">
-                  <i class="icon-base bx bx-calendar icon-26px text-heading"></i>
-                </span>
-                                            <a href="app-calendar.html" class="stretched-link">Calendar</a>
+                                            <span class="dropdown-shortcuts-icon rounded-circle mb-3">
+                                              <i class="icon-base bx bx-calendar icon-26px text-heading"></i>
+                                            </span>
+                                            <a href="#" class="stretched-link">Calendar</a>
                                             <small>Appointments</small>
                                         </div>
                                         <div class="dropdown-shortcuts-item col">
-                <span class="dropdown-shortcuts-icon rounded-circle mb-3">
-                  <i class="icon-base bx bx-food-menu icon-26px text-heading"></i>
-                </span>
-                                            <a href="app-invoice-list.html" class="stretched-link">Invoice App</a>
+                                            <span class="dropdown-shortcuts-icon rounded-circle mb-3">
+                                              <i class="icon-base bx bx-food-menu icon-26px text-heading"></i>
+                                            </span>
+                                            <a href="#" class="stretched-link">Invoice App</a>
                                             <small>Manage Accounts</small>
                                         </div>
                                     </div>
                                     <div class="row row-bordered overflow-visible g-0">
                                         <div class="dropdown-shortcuts-item col">
-                <span class="dropdown-shortcuts-icon rounded-circle mb-3">
-                  <i class="icon-base bx bx-user icon-26px text-heading"></i>
-                </span>
-                                            <a href="app-user-list.html" class="stretched-link">User App</a>
+                                            <span class="dropdown-shortcuts-icon rounded-circle mb-3">
+                                              <i class="icon-base bx bx-user icon-26px text-heading"></i>
+                                            </span>
+                                            <a href="#" class="stretched-link">User App</a>
                                             <small>Manage Users</small>
                                         </div>
                                         <div class="dropdown-shortcuts-item col">
-                <span class="dropdown-shortcuts-icon rounded-circle mb-3">
-                  <i class="icon-base bx bx-check-shield icon-26px text-heading"></i>
-                </span>
-                                            <a href="app-access-roles.html" class="stretched-link">Role Management</a>
+                                            <span class="dropdown-shortcuts-icon rounded-circle mb-3">
+                                              <i class="icon-base bx bx-check-shield icon-26px text-heading"></i>
+                                            </span>
+                                            <a href="#" class="stretched-link">Role Management</a>
                                             <small>Permission</small>
                                         </div>
                                     </div>
-                                    <div class="row row-bordered overflow-visible g-0">
-                                        <div class="dropdown-shortcuts-item col">
-                <span class="dropdown-shortcuts-icon rounded-circle mb-3">
-                  <i class="icon-base bx bx-pie-chart-alt-2 icon-26px text-heading"></i>
-                </span>
-                                            <a href="index.html" class="stretched-link">Dashboard</a>
-                                            <small>User Dashboard</small>
-                                        </div>
-                                        <div class="dropdown-shortcuts-item col">
-                <span class="dropdown-shortcuts-icon rounded-circle mb-3">
-                  <i class="icon-base bx bx-cog icon-26px text-heading"></i>
-                </span>
-                                            <a href="pages-account-settings-account.html" class="stretched-link">Setting</a>
-                                            <small>Account Settings</small>
-                                        </div>
-                                    </div>
-                                    <div class="row row-bordered overflow-visible g-0">
-                                        <div class="dropdown-shortcuts-item col">
-                <span class="dropdown-shortcuts-icon rounded-circle mb-3">
-                  <i class="icon-base bx bx-help-circle icon-26px text-heading"></i>
-                </span>
-                                            <a href="pages-faq.html" class="stretched-link">FAQs</a>
-                                            <small>FAQs &amp; Articles</small>
-                                        </div>
-                                        <div class="dropdown-shortcuts-item col">
-                <span class="dropdown-shortcuts-icon rounded-circle mb-3">
-                  <i class="icon-base bx bx-window-open icon-26px text-heading"></i>
-                </span>
-                                            <a href="modal-examples.html" class="stretched-link">Modals</a>
-                                            <small>Useful Popups</small>
-                                        </div>
-                                    </div>
-                                    <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; right: 0px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 0px;"></div></div></div>
+                                </div>
                             </div>
                         </li>
                         <!-- Quick links -->
